4a4f6089975e17a7291c07d3545cc63218df1655
luisescamilla999 <71411910+luisescamilla999@users.noreply.github.com>
2021-04-12 21:31:33 -0600

    Delete js directory

--- js/controlInicio.js
+++
@@ -1,8 +0,0 @@
-
-function scrollder(){
-    document.querySelector('.caruseldef').scrollLeft += document.querySelector('.caruseldef').offsetWidth;
-}
-
-function scrollizq(){
-    document.querySelector('.caruseldef').scrollLeft -= document.querySelector('.caruseldef').offsetWidth;
-}
