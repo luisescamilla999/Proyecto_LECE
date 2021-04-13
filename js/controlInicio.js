af89efdcb47cd69b8a076bbab7b55c4608419496
luisescamilla999 <71411910+luisescamilla999@users.noreply.github.com>
2021-04-12 21:23:36 -0600

    Add files via upload
    segunda entrega del proyecto

---
+++ js/controlInicio.js
@@ -0,0 +1,8 @@
+
+function scrollder(){
+    document.querySelector('.caruseldef').scrollLeft += document.querySelector('.caruseldef').offsetWidth;
+}
+
+function scrollizq(){
+    document.querySelector('.caruseldef').scrollLeft -= document.querySelector('.caruseldef').offsetWidth;
+}
